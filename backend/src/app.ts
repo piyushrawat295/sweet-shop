61460aa91aa4205fb9e28d315fbe9744a7f4d74f
Piyush Rawat <unexpected086@gmail.com>
2025-12-14 15:39:03 +0530

    Check6

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,12 +6,12 @@ import { errorMiddleware } from "./middlewares/error.middleware";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-app.get("/api/health", (req, res) => {
-  res.json({ status: "OK" });
-});
+// app.get("/ping", (req, res) => {
+//   res.send("pong");
+// });
+// app.get("/api/health", (req, res) => {
+//   res.json({ status: "OK" });
+// });
 app.use("/api", routes);
 app.use(errorMiddleware);
 
