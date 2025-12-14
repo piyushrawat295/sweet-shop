5fc33bf7d8351527ccff99d8062854c4497b317a
Piyush Rawat <unexpected086@gmail.com>
2025-12-14 15:30:53 +0530

    check5

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,9 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log("Server running on", PORT);
+  console.log(`Server running on ${PORT}`);
 });
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
